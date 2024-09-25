b280ee7f64af5e2faf360d74a7a804035965ebf1
ahamadoubah <bah.t@northeastern.edu>
2024-09-24 22:25:43 -0400

    a1

---
+++ src/Kanbas/Courses/Home/index.tsx
@@ -0,0 +1,20 @@
+import Modules from "../Modules";
+import CourseStatus from ".";
+import Assignments from "../Assignments";
+export default function Home() {
+  return (
+    <table id="wd-home">
+      <tr>
+        <td valign="top">
+          <Modules />
+        </td>
+        <td valign="top">
+          <CourseStatus />
+        </td>
+        <td valign="top">
+          <Assignments />
+        </td>
+      </tr>
+    </table>
+  );
+}
