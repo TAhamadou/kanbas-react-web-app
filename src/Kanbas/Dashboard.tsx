0b7bfdce625f2c98c66ba4db9eb4270f5f11242b
ahamadoubah <bah.t@northeastern.edu>
2024-12-11 00:59:48 -0500

    ;

--- src/Kanbas/Dashboard.tsx
+++ src/Kanbas/Dashboard.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
